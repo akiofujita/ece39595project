17458612184c6dc578dd0fbed30541df056d0dd0
JamesZCT <70852638+JamesZCT@users.noreply.github.com>
2021-10-01 06:15:17 -0400

    Player.java finished

---
+++ afujita/src/Player.java
@@ -0,0 +1,16 @@
+package afujita.src;
+
+public class Player extends Creature {
+    
+    public Player() {
+        System.out.println("Player created")
+    }
+
+    public void setWeapon(Item _sword) {
+        System.out.println("Weapon set");
+    }
+
+    public void setArmor(Item _armor) {
+        System.out.println("Armor set");
+    }
+}
