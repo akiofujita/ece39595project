14b602e43cc046fb5340115864b049cb76bc2cb7
Akio K Fujita <afujita@ecegrid-thin1.ecn.purdue.edu>
2021-10-01 06:00:28 -0400

    Made dungeon a singleton, changes to formatting

--- afujita/src/Dungeon.java
+++ afujita/src/Dungeon.java
@@ -1,8 +1,31 @@
 package afujita.src;
 
-public class Dungeon {
-    public Dungeon(String _name, int _width, int _gameHeight) {
-        System.out.println("Dungeon Room created");
+public class Dungeon extends Displayable {
+
+    private static Dungeon myDungeon = null;
+    private String name;
+    private int width;
+    private int topHeight;
+    private int gameHeight;
+    private int bottomHeight;
+
+    private Dungeon(String _name, int _width, int _topHeight, int _gameHeight, int _bottomHeight) {
+        name = _name;
+        width = _width;
+        topHeight = _topHeight;
+        gameHeight = _gameHeight;
+        bottomHeight = _bottomHeight;
+    }
+
+    public static Dungeon buildDungeon(String _name, int _width, int _topHeight, int _gameHeight, int _bottomHeight) {
+        if (myDungeon == null) {
+            myDungeon = new Dungeon(_name, _width, _topHeight, _gameHeight, _bottomHeight);
+            System.out.println("Dungeon Room created");
+        }
+        else {
+            System.out.println("Dungeon Room created already");
+        }
+        return myDungeon;
     }
 
     public void addRoom(Room) {
--- afujita/src/Structure.java
+++ afujita/src/Structure.java
@@ -1,8 +1,12 @@
 package afujita.src;
 
+<<<<<<< HEAD
 public class Structure extends Displayable{
+=======
+public class Structure extends Displayable {
+>>>>>>> Made dungeon a singleton, changes to formatting
     
     public Structure() {
-        System.out.println("Structure created (extends Displayable");
+        System.out.println("Structure created (extends Displayable)");
     }
 }
