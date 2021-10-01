e7149d4a4d82e88fea5f95ca111b1356512e2545
JamesZCT <70852638+JamesZCT@users.noreply.github.com>
2021-10-01 04:38:43 -0400

    starting classes, Displayable.java in progress

---
+++ Displayable.java
@@ -0,0 +1,24 @@
+public class Displayable {
+
+    public Displayable() {
+
+    }
+
+    public void setInvisible() {
+
+    }
+
+    public void setVisible() {
+        
+    }
+
+    public void setMaxHit(int maxHit) {
+        
+    }
+
+    public void setHpMove(int hpMoves) {
+        
+    }
+
+    public void 
+}
