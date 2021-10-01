54b0df2c26e5ebdb27747a68df39310f55ae2221
JamesZCT <70852638+JamesZCT@users.noreply.github.com>
2021-10-01 04:55:55 -0400

    Displayable.java finished

--- afujita/src/Displayable.java
+++ afujita/src/Displayable.java
@@ -6,50 +6,46 @@ public class Displayable {
     }
 
     public void setInvisible() {
-        System.out.println("Invisible")
+        System.out.println("Invisible set")
     }
 
     public void setVisible() {
-        
+        System.out.println("Visible set");
     }
 
     public void setMaxHit(int maxHit) {
-        
+        System.out.println("maxHit set"); 
     }
 
     public void setHpMove(int hpMoves) {
-        
+        System.out.println("HpMove set");
     }
 
     public void setHp(int Hp) {
-
+        System.out.println("Hp set");
     }
 
     public void setType(char t) {
-
-    }
-
-    public void setType(char t) {
-        System.out.println("   type: " + type);
+        System.out.println("Type set");
     }
 
     public void setIntValue(int v){
-        System.out.println("   intValue: " + intValue);
+        System.out.println("IntValue set");
     }
 
     public void setPosX(int x){
-        System.out.println("   posX: " + x);
+        System.out.println("PosX set");
     }
 
     public void setPosY(int y){
-        System.out.println("   posY: " + y);
+        System.out.println("PosY set");
     }
 
     public void setWidth(int x){
-        System.out.println("   width: " + x);
+        System.out.println("Width set");
     }
 
     public void setHeight(int y){
-        System.out.println("   height: " + y);
+        System.out.println("Height set");   
     }
 }
