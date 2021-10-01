19960d4db1478c45898eeefc28395ca72b6d02b2
JamesZCT <70852638+JamesZCT@users.noreply.github.com>
2021-10-01 06:18:45 -0400

    Passage.java finished

---
+++ afujita/src/Passage.java
@@ -0,0 +1,16 @@
+package afujita.src;
+
+public class Passage extends Structure {
+    
+    public Passage() {
+        System.out.println("Passage created");
+    }
+
+    public void setName(String) {
+        System.out.println("Name set");
+    }
+
+    public void setID(int _room1, int _room2) {
+        System.out.println("ID set for room");
+    }
+}
