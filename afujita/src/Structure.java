291dce771d0b5ce896bcbd7917a2c1043080048e
Akio K Fujita <afujita@ecegrid-thin1.ecn.purdue.edu>
2021-10-01 06:04:18 -0400

    Structure.java merge

--- afujita/src/Structure.java
+++ afujita/src/Structure.java
@@ -1,10 +1,6 @@
 package afujita.src;
 
-<<<<<<< HEAD
 public class Structure extends Displayable{
-=======
-public class Structure extends Displayable {
->>>>>>> Made dungeon a singleton, changes to formatting
     
     public Structure() {
         System.out.println("Structure created (extends Displayable)");
