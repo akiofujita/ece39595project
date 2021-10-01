d93e15a5e75faaa02016ae6c3bd2ab05a34f70a8
JamesZCT <70852638+JamesZCT@users.noreply.github.com>
2021-10-01 06:07:50 -0400

    I've created a Monster.java

---
+++ afujita/src/Monster.java
@@ -0,0 +1,17 @@
+package afujita.src;
+
+public class Monster extends Creature {
+
+    public Monster() {
+        System.out.println("Monster created");
+    }
+
+    public void setName(String) {
+        System.out.println("Name set");
+    }
+
+    public void setID(int room, int seriel) {
+        System.out.println("ID set");
+    }
+    
+}
