373b3dd90858650c32dd4bc09abcf6dc682e89b8
Akio Fujita <afujita@purdue.edu>
2021-10-27 02:47:08 -0400

    Reorganize files to game folder. Import necessary files from asciiexample.

--- step2/afujita/src/game/Test.java
+++ step2/afujita/src/game/Test.java
@@ -8,11 +8,39 @@ import javax.xml.parsers.SAXParserFactory;
 
 import org.xml.sax.SAXException;
 
-import src.CreatureAction;
-import src.Monster;
-import src.Player;
+public class Test implements Runnable {
 
-public class Test {
+    private static final int DEBUG = 0;
+    private boolean isRunning;
+    public static final int FRAMESPERSECOND = 60;
+    public static final int TIMEPERLOOP = 1000000000 / FRAMESPERSECOND;
+    private static ObjectDisplayGrid displayGrid = null;
+    private Thread keyStrokePrinter;
+    private static final int WIDTH = 80;
+    private static final int HEIGHT = 40;
+
+    public Test(int width, int height) {
+        displayGrid = new ObjectDisplayGrid(width, height);
+    }
+
+    @Override
+    public void run() {
+        displayGrid.fireUp();
+        for (int step = 1; step < WIDTH / 2; step *= 2) {
+            for (int i = 0; i < WIDTH; i += step) {
+                for (int j = 0; j < HEIGHT; j += step) {
+                    displayGrid.addObjectToDisplay(new Char('X'), i, j);
+                }
+            }
+
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace(System.err);
+            }
+            displayGrid.initializeDisplay();
+        }
+    }
 
     public static void main(String[] args) {
 
